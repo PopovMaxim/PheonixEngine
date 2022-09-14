b8c0ec5b8dd259717cbadf8c1b592f1ee76c0081
Maxim Popov <popov.maxim.34@yandex.ru>
2022-09-14 11:04:24 +0300

    Фикс быстрого бонуса

--- app/Modules/Dashboard/Resources/views/index.blade.php
+++ app/Modules/Dashboard/Resources/views/index.blade.php
@@ -1,38 +1,40 @@
 @extends('dashboard::layouts.master')
 
 @push('js')
-<script src="{{ asset('assets/js/plugins/easy-pie-chart/jquery.easypiechart.min.js') }}"></script>
-<script>
-    var countDownDate = new Date("{{ $quick_bonus['quick_bonus_end'] }}").getTime();
+    @if ($quick_bonus['quick_bonus_end']->addDays(2) >= now() || ($quick_bonus['current_percent'] >= 100 && !request()->user()->quick_bonus_accepted))
+        <script src="{{ asset('assets/js/plugins/easy-pie-chart/jquery.easypiechart.min.js') }}"></script>
+        <script>
+            var countDownDate = new Date("{{ $quick_bonus['quick_bonus_end'] }}").getTime();
 
-    var x = setInterval(function() {
-        var now = new Date().getTime();
-        var distance = countDownDate - now;
+            var x = setInterval(function() {
+                var now = new Date().getTime();
+                var distance = countDownDate - now;
 
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        if (hours < 10) { hours = '0' + hours; }
-        if (minutes < 10) { minutes = '0' + minutes; } 
-        if (seconds < 10) { seconds = '0' + seconds; } 
-        
-        document.getElementById("days").innerHTML = days;
-        document.getElementById("hours").innerHTML = hours;
-        document.getElementById("minutes").innerHTML = minutes;
-        document.getElementById("seconds").innerHTML = seconds;
+                if (hours < 10) { hours = '0' + hours; }
+                if (minutes < 10) { minutes = '0' + minutes; } 
+                if (seconds < 10) { seconds = '0' + seconds; } 
+                
+                document.getElementById("days").innerHTML = days;
+                document.getElementById("hours").innerHTML = hours;
+                document.getElementById("minutes").innerHTML = minutes;
+                document.getElementById("seconds").innerHTML = seconds;
 
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("days").innerHTML = 0;
-            document.getElementById("hours").innerHTML = '00';
-            document.getElementById("minutes").innerHTML = '00';
-            document.getElementById("seconds").innerHTML = '00';
-        }
-    }, 1000);
-</script>
-<script>Dashmix.helpersOnLoad(['jq-easy-pie-chart']);</script>
+                if (distance < 0) {
+                    clearInterval(x);
+                    document.getElementById("days").innerHTML = 0;
+                    document.getElementById("hours").innerHTML = '00';
+                    document.getElementById("minutes").innerHTML = '00';
+                    document.getElementById("seconds").innerHTML = '00';
+                }
+            }, 1000);
+        </script>
+        <script>Dashmix.helpersOnLoad(['jq-easy-pie-chart']);</script>
+    @endif
 @endpush
 
 @section('content')

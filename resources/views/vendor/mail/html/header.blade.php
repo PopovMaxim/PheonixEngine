0e990bcf9852c435deadc6a1945a6aa524d1ae4a
Максим Попов <popov.maxim.34@yandex.ru>
2022-10-08 16:26:15 +0300

    Фикс

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('assets/media/logos/logo-short.png') }}" class="logo" style="width: 200px;" alt="{{ config('app.name') }}">
+<img src="{{ asset('assets/media/logos/logo-short.png') }}" class="logo" style="width: 200px !important;" alt="{{ config('app.name') }}">
 </a>
 </td>
 </tr>

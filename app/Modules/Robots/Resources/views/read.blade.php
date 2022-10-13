d4203d281a17616b58f09ade2c4a3a7e179395e8
Maxim Popov <popov.maxim.34@yandex.ru>
2022-10-13 17:26:48 +0300

    Апдейт раздела подписок

--- app/Modules/Robots/Resources/views/read.blade.php
+++ app/Modules/Robots/Resources/views/read.blade.php
@@ -18,35 +18,84 @@
         <div class="row d-flex justify-content-center">
             <div class="col-md-12">
                 @if (!$product_key)
-                    <form method="POST">
-                        @csrf
-                        <div class="block block-rounded">
-                            <div class="block-content">
-                                <div class="row justify-content-center py-sm-3 py-md-5">
-                                    <div class="col-sm-10 col-md-8">
-                                        <div class="mb-4">
-                                            <label class="form-label" for="account-number">Номер счёта</label>
-                                            <input type="text" class="form-control form-control-alt @error('account_number') is-invalid @enderror" id="account-number" name="account_number" value="{{ old('account_number') }}" placeholder="Введите номер счёта">
-                                            @error('account_number')
-                                                <div class="invalid-feedback animated fadeIn">{{ $message }}</div>
-                                            @enderror
+                    <div class="row d-flex justify-content-between">
+                        <div class="col-md-6">
+                            <form method="POST">
+                                @csrf
+                                <div class="block block-rounded">
+                                    <div class="block-content">
+                                        <div class="p-4">
+                                            <div class="mb-4">
+                                                <label class="form-label" for="account-number">Номер счёта</label>
+                                                <input type="text" class="form-control form-control-alt @error('account_number') is-invalid @enderror" id="account-number" name="account_number" value="{{ old('account_number') }}" placeholder="Введите номер счёта">
+                                                @error('account_number')
+                                                    <div class="invalid-feedback animated fadeIn">{{ $message }}</div>
+                                                @enderror
+                                            </div>
+                                            <div class="mb-4">
+                                                <div class="form-check">
+                                                    <input class="form-check-input @error('accept') is-invalid @enderror" type="checkbox" value="1" id="accept" name="accept">
+                                                    <label class="form-check-label" for="accept">Я уверен(а) что хочу активировать данный номер счёта.</label>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div class="mb-4">
-                                            <div class="form-check">
-                                                <input class="form-check-input @error('accept') is-invalid @enderror" type="checkbox" value="1" id="accept" name="accept">
-                                                <label class="form-check-label" for="accept">Я уверен(а) что хочу активировать данный номер счёта.</label>
+                                    </div>
+                                    <div class="block-content block-content-full block-content-sm bg-body-light text-center">
+                                        <button type="submit" class="btn btn-sm btn-alt-primary">
+                                            <i class="fa fa-check opacity-50 me-1"></i> Активировать номер счёта
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-md-5">
+                            <div>
+                                <h4>Часто задаваемые вопросы</h4>
+                                <div id="accordion" role="tablist" aria-multiselectable="true">
+                                    <div class="block block-transparent mb-0">
+                                        <div class="block-header ps-0 pt-0" role="tab" id="h1">
+                                            <a class="fw-semibold collapsed" data-bs-toggle="collapse" data-bs-parent="#accordion" href="#q1" aria-expanded="true" aria-controls="q1">Как зарегистрироваться на Roboforex?</a>
+                                        </div>
+                                        <div id="q1" class="collapse" role="tabpanel" aria-labelledby="h1" data-bs-parent="#accordion" style="">
+                                            <div class="block-content ps-0 pt-0 fs-sm pb-3">
+                                                <a href="{{ asset('pdf/robo-register.pdf') }}" target="_black"><i class="fa fa-external-link"></i> Открыть инструкцию</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="block block-transparent mb-0">
+                                        <div class="block-header ps-0 pt-0" role="tab" id="h2">
+                                            <a class="fw-semibold collapsed" data-bs-toggle="collapse" data-bs-parent="#accordion" href="#q2" aria-expanded="true" aria-controls="q2">Как создать новый аккаунт на Roboforex в партнёрской сети Pheonix?</a>
+                                        </div>
+                                        <div id="q2" class="collapse" role="tabpanel" aria-labelledby="h2" data-bs-parent="#accordion" style="">
+                                            <div class="block-content ps-0 pt-0 fs-sm pb-3">
+                                                <a href="{{ asset('pdf/robo-new-acc-register.pdf') }}" target="_black"><i class="fa fa-external-link"></i> Открыть инструкцию</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="block block-transparent mb-0">
+                                        <div class="block-header ps-0 pt-0" role="tab" id="h3">
+                                            <a class="fw-semibold collapsed" data-bs-toggle="collapse" data-bs-parent="#accordion" href="#q3" aria-expanded="true" aria-controls="q3">Как создать счёт для Insider 1.61?</a>
+                                        </div>
+                                        <div id="q3" class="collapse" role="tabpanel" aria-labelledby="h3" data-bs-parent="#accordion" style="">
+                                            <div class="block-content ps-0 pt-0 fs-sm pb-3">
+                                                <a href="{{ asset('pdf/robo-insider-acc-register.pdf') }}" target="_black"><i class="fa fa-external-link"></i> Открыть инструкцию</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="block block-transparent mb-0">
+                                        <div class="block-header ps-0 pt-0" role="tab" id="h3">
+                                            <a class="fw-semibold collapsed" data-bs-toggle="collapse" data-bs-parent="#accordion" href="#q4" aria-expanded="true" aria-controls="q4">Как открыть счёт Roboforex MT4 ProCent?</a>
+                                        </div>
+                                        <div id="q4" class="collapse" role="tabpanel" aria-labelledby="h4" data-bs-parent="#accordion" style="">
+                                            <div class="block-content ps-0 pt-0 fs-sm pb-3">
+                                                <a href="{{ asset('pdf/robo-procent-register.pdf') }}" target="_black"><i class="fa fa-external-link"></i> Открыть инструкцию</a>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="block-content block-content-full block-content-sm bg-body-light text-center">
-                                <button type="submit" class="btn btn-sm btn-alt-primary">
-                                    <i class="fa fa-check opacity-50 me-1"></i> Активировать номер счёта
-                                </button>
-                            </div>
                         </div>
-                    </form>
+                    </div>
                 @else
                     <div class="row items-push">
                         <div class="col-md-4">

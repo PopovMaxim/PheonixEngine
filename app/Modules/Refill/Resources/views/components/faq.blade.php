312dd4975f6591f04a8ac26d5ec1b19d53f00fac
Maxim Popov <popov.maxim.34@yandex.ru>
2022-10-13 19:06:11 +0300

    Апдейт пополнения

--- app/Modules/Refill/Resources/views/components/faq.blade.php
+++ app/Modules/Refill/Resources/views/components/faq.blade.php
@@ -22,19 +22,49 @@
                 </div>
             </div>
         </div>
-    </div>        
+        <div class="block block-transparent mb-0">
+            <div class="block-header ps-0 pt-0" role="tab" id="header_4">
+                <a class="fw-semibold collapsed" data-bs-toggle="collapse" data-bs-parent="#accordion" href="#text_4" aria-expanded="true" aria-controls="text_4">Как пополнить личный кабинет?</a>
+            </div>
+            <div id="text_4" class="collapse" role="tabpanel" aria-labelledby="header_4" data-bs-parent="#accordion" style="">
+                <div class="block-content ps-0 pt-0 fs-sm">
+                    <video
+                        id="my-video"
+                        class="video-js vjs-theme-fantasy vjs-16-9"
+                        controls
+                        preload="auto"
+                        poster="{{ asset('assets/media/covers/refill.jpg') }}"
+                        data-setup="{}"
+                    >
+                        <source src="{{ asset('videos/refill.mp4') }}" type="video/mp4" />
+                        <p class="vjs-no-js">Для просмотра этого видео включите JavaScript и рассмотрите возможность обновления веб-браузера до
+                            <a href="https://videojs.com/html5-video-support/" target="_blank">поддерживающего HTML5 видео.</a>
+                        </p>
+                    </video>
+                </div>
+            </div>
+        </div>
+        
+        <div class="block block-transparent mb-0">
+            <div class="block-header ps-0 pt-0" role="tab" id="h2">
+                <a class="fw-semibold collapsed" data-bs-toggle="collapse" data-bs-parent="#accordion" href="#q2" aria-expanded="true" aria-controls="q2">Как зарегистрироваться на криптобирже ByBit?</a>
+            </div>
+            <div id="q2" class="collapse" role="tabpanel" aria-labelledby="h2" data-bs-parent="#accordion" style="">
+                <div class="block-content ps-0 pt-0 fs-sm pb-3">
+                    <a href="{{ asset('pdf/bybit/register.pdf') }}" target="_black"><i class="fa fa-external-link"></i> Открыть инструкцию</a>
+                </div>
+            </div>
+        </div>
 
-    <video
-        id="my-video"
-        class="video-js vjs-theme-fantasy vjs-16-9"
-        controls
-        preload="auto"
-        poster="{{ asset('assets/media/covers/refill.jpg') }}"
-        data-setup="{}"
-    >
-        <source src="{{ asset('videos/refill.mp4') }}" type="video/mp4" />
-        <p class="vjs-no-js">Для просмотра этого видео включите JavaScript и рассмотрите возможность обновления веб-браузера до
-            <a href="https://videojs.com/html5-video-support/" target="_blank">поддерживающего HTML5 видео.</a>
-        </p>
-    </video>
+        <div class="block block-transparent mb-0">
+            <div class="block-header ps-0 pt-0" role="tab" id="h3">
+                <a class="fw-semibold collapsed" data-bs-toggle="collapse" data-bs-parent="#accordion" href="#q3" aria-expanded="true" aria-controls="q3">Как купить криптовалюту на криптобирже ByBit?</a>
+            </div>
+            <div id="q3" class="collapse" role="tabpanel" aria-labelledby="h3" data-bs-parent="#accordion" style="">
+                <div class="block-content ps-0 pt-0 fs-sm pb-3">
+                    <a href="{{ asset('pdf/bybit/buy.pdf') }}" target="_black"><i class="fa fa-external-link"></i> Открыть инструкцию</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>

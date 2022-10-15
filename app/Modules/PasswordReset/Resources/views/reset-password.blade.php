e17b9ed0766e85881fb864967a8e49a3a273a773
Maxim Popov <popov.maxim.34@yandex.ru>
2022-10-15 11:48:01 +0300

    Фикс восстановления пароля

---
+++ app/Modules/PasswordReset/Resources/views/reset-password.blade.php
@@ -0,0 +1,64 @@
+@extends('passwordreset::layouts.master')
+
+@push('js')
+@endpush
+
+@push('css')
+    <style>
+        body {
+            background-image: url('assets/media/welcome/hero2.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: top;
+        }
+    </style>
+@endpush
+
+@section('content')
+<div class="bg-primary-dark-op">
+    <div class="hero-static d-flex align-items-center justify-content-center">
+        <div class="block block-rounded w-100 overflow-hidden p-3"style="max-width: 450px;">
+            <div class="block-content block-content-full">
+                <div class="text-center">
+                    <a class="fw-bold fs-1" href="{{ route('register') }}">
+                        <img src="{{ asset('assets/media/logos/logo-short.png') }}" class="img-fluid w-50 mb-5" />
+                    </a>
+                    <p class="text-uppercase fw-bold fs-sm text-muted">Восстановление пароля</p>
+                </div>
+
+                <form action="{{ route('password.update') }}" method="POST">
+                    @csrf
+                    <input type="password" name="password" hidden />
+                    <input type="hidden" name="token" value="{{ $token }}" />
+                    <input type="hidden" name="email" value="{{ request()->get('email') }}" />
+
+                    <div class="py-3">
+                        <input type="password" class="form-control form-control-lg form-control-alt @error('password') is-invalid @enderror" id="password" name="password" placeholder="Новый пароль">
+                        @error('password')
+                            <div class="invalid-feedback animated fadeIn">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="pb-3">
+                        <input type="password" class="form-control form-control-lg form-control-alt @error('password_confirmation') is-invalid @enderror" id="password_confirmation" name="password_confirmation" placeholder="Подтвердите новый пароль">
+                        @error('password_confirmation')
+                            <div class="invalid-feedback animated fadeIn">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="text-center mb-4">
+                        <button type="submit" class="btn w-100 btn-lg btn-hero btn-primary">
+                            <i class="fa fa-fw fa-reply opacity-50 me-1"></i> Обновить пароль
+                        </button>
+                    </div>
+                    
+                    <p class="mt-3 mb-0 d-lg-flex justify-content-lg-between">
+                        <a class="btn btn-sm btn-alt-secondary d-block d-lg-inline-block mb-1" href="{{ route('login') }}">
+                            <i class="fa fa-arrow-left opacity-50 me-1"></i> Назад
+                        </a>
+                    </p>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- app/Modules/PasswordReset/Routes/web.php
+++ app/Modules/PasswordReset/Routes/web.php
@@ -11,8 +11,6 @@ Route::get('/forgot-password', function () {
 })->middleware('guest')->name('password.request');
 
 Route::post('/forgot-password', function (Request $request) {
-
-    //dd(config('mail'));
     $request->validate(['email' => 'required|email'], [
         'email.required' => 'Вы должны ввести адрес электронной почты',
         'email.email' => 'Данные не соответствуют формату электронной почты'
@@ -24,19 +22,24 @@ Route::post('/forgot-password', function (Request $request) {
     $status = Password::sendResetLink($email);
  
     return $status === Password::RESET_LINK_SENT
-                ? back()->with(['status' => __($status)])
+                ? back()->with('status', [
+                    'title' => 'Сброс пароля',
+                    'text' => 'Сообщение с ссылкой на сброс пароля отправлена.',
+                    'type' => 'success'
+                ])
                 : back()->withErrors(['email' => __($status)]);
 })->middleware('guest')->name('password.email');
 
 Route::get('/reset-password/{token}', function ($token) {
-    return view('auth.reset-password', ['token' => $token]);
-})->middleware('guest')->name('auth.reset-password');
+    return view('passwordreset::reset-password', ['token' => $token]);
+})->middleware('guest')->name('password.reset');
 
 Route::post('/reset-password', function (Request $request) {
     $request->validate([
         'token' => 'required',
         'email' => 'required|email',
         'password' => 'required|min:8|confirmed',
+        'password_confirmation' => 'required',
     ]);
  
     $status = Password::reset(
@@ -53,6 +56,10 @@ Route::post('/reset-password', function (Request $request) {
     );
  
     return $status === Password::PASSWORD_RESET
-                ? redirect()->route('login')->with('status', __($status))
+                ? redirect()->route('login')->with('status', [
+                    'title' => 'Сброс пароля',
+                    'text' => 'Вы успешно сбросили пароль.',
+                    'type' => 'success'
+                ])
                 : back()->withErrors(['email' => [__($status)]]);
 })->middleware('guest')->name('password.update');

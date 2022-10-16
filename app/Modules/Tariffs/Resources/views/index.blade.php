cca2784f30eaaa6a034fea8bcc7dcc986b1109d2
Maxim Popov <popov.maxim.34@yandex.ru>
2022-10-16 13:07:41 +0300

    добавлено пояснение в тарифы

--- app/Modules/Tariffs/Resources/views/index.blade.php
+++ app/Modules/Tariffs/Resources/views/index.blade.php
@@ -4,4 +4,7 @@
     @livewire('tariffs', [
         'line' => $line['id']
     ])
+    <div class="d-flex justify-content-center my-5">
+        <div class="text-center text-muted">Чтобы вам было начислено вознаграждению за рекомендацию <br/> Вы должны иметь действующую лицензию на продукт.</div>
+    </div>
 @endsection

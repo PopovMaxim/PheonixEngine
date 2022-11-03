fbd694651058604c0814afa6c2fbc6c86e45d7ec
Максим Попов <popov.maxim.34@yandex.ru>
2022-11-03 15:30:53 +0300

    Fix api and upload latest insider.

--- app/Modules/Notifications/Routes/api.php
+++ app/Modules/Notifications/Routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Modules\Notifications\Entities\Notification;
 
@@ -25,23 +26,36 @@ Route::middleware('api')->prefix('v1')->group(function () {
 
         $read_state = $request->input('read_state');
 
-        $notifications = Notification::query()
-            ->select(['id', 'notifiable_id as user_id', 'data', 'read_at', 'created_at'])
-            ->where('notifiable_id', $request->input('user_id'))
-            ->where('notifiable_type', 'App\Models\User')
-            ->orderBy('created_at', $order)
-            ->when($read_state, function ($query, $read_state)
-            {
-                if ($read_state)
+        $user = User::query()
+            ->where('telegram_id', $request->input('telegram_id'))
+            ->first();
+
+        $limit = 10;
+
+        if ($request->has('limit')) {
+            $limit = $request->input('limit');
+        }
+
+        if ($user) {
+            $notifications = Notification::query()
+                ->select(['id', 'notifiable_id as user_id', 'data', 'read_at', 'created_at'])
+                ->where('notifiable_id', $user['id'])
+                ->where('notifiable_type', 'App\Models\User')
+                ->orderBy('created_at', $order)
+                ->when($read_state, function ($query, $read_state)
                 {
-                    $query->whereNotNull('read_at');
-                }
-                else if (!$read_state)
-                {
-                    $query->whereNull('read_at');
-                }
-            })
-            ->get();
+                    if ($read_state)
+                    {
+                        $query->whereNotNull('read_at');
+                    }
+                    else if (!$read_state)
+                    {
+                        $query->whereNull('read_at');
+                    }
+                })
+                ->limit($limit)
+                ->get();
+        }
 
         return $notifications;
     });
